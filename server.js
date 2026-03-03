f08a7dc58d3b6967952aa4b8f191e8c42d8db676
НовыйИмя <Email>
2026-03-03 20:37:02 +0100

    cartItemGrid

--- server.js
+++ server.js
@@ -908,9 +908,10 @@ app.post("/orders/confirm", async (req, res) => {
     if (first?.productKey) {
       const prod = await Product.findOne(
         { productKey: String(first.productKey) },
-        { orderImgUrl: 1, cardBgUrl: 1 }
+        { cardBgUrl: 1 }
       ).lean();
-      bgUrl = String(prod?.orderImgUrl || prod?.cardBgUrl || "");
+
+      bgUrl = String(prod?.cardBgUrl || "");
     }
 
     // 5) Собрать items snapshot в твою структуру (product -> flavors[])
